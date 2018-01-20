5f211d5f75e3e762b25cac67ff3cd58f55b7e9ab
fanhl <fanhonglin@klicen.com>
2018-01-20 19:27:13 +0800

    MainController

---
+++ src/main/java/com/fanhl/springdemo/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.fanhl.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 主控件器
+ *
+ * @author fanhl
+ */
+@Controller
+public class MainController {
+    /**
+     * 首页
+     *
+     * @return 首页
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index2";
+    }
+}
